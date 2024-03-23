1e82d00859fc8d6d276251a25ef9c02d7d408454
Kailash <h.kailash1501@gmail.com>
2024-03-23 14:46:14 +0530

    fix UT

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Docker and Aws sucks
+          Learn React
         </a>
       </header>
     </div>
